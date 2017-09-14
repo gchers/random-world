d7e935b1bc370a429ba48e9f3e1c9831988e85ab
Giovanni Cherubin <giovanni.cherubin@gmail.com>
2017-09-15 00:31:56 +0100

    Improve k-NN ncm code

--- src/ncm.rs
+++ src/ncm.rs
@@ -1,17 +1,17 @@
+use std::f64;
+use std::cmp::min;
 use rusty_machine::linalg::{Matrix, BaseMatrix};
-use std::cmp::Ordering::Equal;
 
 
 //// Should use
 //// https://athemathmo.github.io/rulinalg/doc/rulinalg/vector/struct.Vector.html#method.metric
-pub fn euclidean_distance(v1: &[f64], v2: &[f64]) -> f64 {
+fn euclidean_distance(v1: &[f64], v2: &[f64]) -> f64 {
     let dist: f64 = v1.iter()
                       .zip(v2.iter())
                       .map(|(x,y)| (x - y).powi(2))
                       .sum();
     dist.sqrt()
 }
-
 
 pub trait NonConformityScorer<T> {
     /// Compute a k-NN nonconformity score on the i-th input
@@ -34,51 +34,24 @@ impl NonConformityScorer<Matrix<f64>> for KNN {
     fn score(&self, i: usize, inputs: &Matrix<f64>) -> f64 {
         let input = inputs.get_row(i).expect("Invalid index");
 
-        //inf = 1000; // XXX
-        //let mut smallest: (f64, usize) = (inf, 0);
-        //let mut k_smallest = vec![inf; k];
-        let mut distances = inputs.iter_rows()
+        let distances = inputs.iter_rows()
                                   .enumerate()
                                   .filter(|&(j, _)| j != i)
                                   .map(|(_, x)| (self.distance)(x, input))
-                                  //.filter(|d| { if d > smallest[0] {
-                                  //                  k_smallest[smallest[1]] = d;
-                                  //                  // Recompute idx to smallest
-                                  //                  smallest[1] = d;
-                                  //                  smallest[0] = ?;
-                                  //            }
                                   .collect::<Vec<_>>();
 
-        distances.sort_by(|a, b| a.partial_cmp(b).unwrap_or(Equal));
+        let k = min(self.k, distances.len());
+        let mut k_smallest = vec![f64::INFINITY; k];
 
-        distances.iter()
-             .take(self.k)
-             .sum()
+        for d in distances {
+            for i in 0..k {
+                if d < k_smallest[i] {
+                    k_smallest[i] = d;
+                    break;
+                }
+            }
+        }
+        k_smallest.iter()
+                  .sum()
     }
 }
-
-
-//trait Klargest {
-//    fn k_largest(&self, k: usize);
-//}
-
-///// Cheap method to get k largest numbers in iterator (without sorting).
-//impl Klargest for Iterator<f64> {
-//    fn k_largest(&self, k: usize) -> Iterator<f64> {
-//        let mut out: &[f64] = self.take(k);
-//
-//        for i in (k..v.len()) {
-//            for j in (0..out) {
-//                if let x = Some(out) {
-//                    if x > v[i] {
-//                        v[i] = x;
-//                        continue;
-//                    }
-//                } else {
-//                    out[j] = Some(v[i]);
-//                    continue;
-//                }
-//            }
-//        }
-//        out
-//    }
