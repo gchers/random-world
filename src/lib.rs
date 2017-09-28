401493448e4eaca2007219af09c9be9b82fd3a49
Giovanni Cherubin <giovanni.cherubin@gmail.com>
2017-09-28 16:56:35 +0100

    Improve readability of score() for KNN

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 extern crate rand;
 extern crate rayon;
+extern crate lazysort;
 extern crate pcg_rand;
 extern crate itertools;
 extern crate rusty_machine;
+extern crate ordered_float;
 
 pub mod cp;
 pub mod ncm;
--- src/ncm.rs
+++ src/ncm.rs
@@ -1,5 +1,7 @@
 use std::f64;
 use std::cmp::min;
+use ordered_float::OrderedFloat;
+use lazysort::Sorted;
 
 // TODO: Should use
 // https://athemathmo.github.io/rulinalg/doc/rulinalg/vector/struct.Vector.html#method.metric
@@ -32,26 +34,19 @@ impl KNN<Vec<f64>> {
 impl<T: Sync> NonConformityScorer<T> for KNN<T> {
     fn score(&self, i: usize, inputs: &[T]) -> f64 {
         let input = &inputs[i];
+        let k = min(self.k, inputs.len()-1);
 
-        let distances = inputs.iter()
-                              .enumerate()
-                              .filter(|&(j, _)| j != i)
-                              .map(|(_, x)| (self.distance)(x, input))
-                              .collect::<Vec<_>>();
-
-        let k = min(self.k, distances.len());
-        let mut k_smallest = vec![f64::INFINITY; k];
-
-        for d in distances {
-            for i in 0..k {
-                if d < k_smallest[i] {
-                    k_smallest[i] = d;
-                    break;
-                }
-            }
-        }
-        k_smallest.iter()
-                  .sum()
+        inputs.iter()
+              .enumerate()
+              .filter(|&(j, _)| j != i)
+              /* Compute distances and convert to OrderedFloat for sorting */
+              .map(|(_, x)| OrderedFloat((self.distance)(x, input)))
+              /* Lazy sort: faster because generally k << inputs.len() */
+              .sorted()
+              .take(k)
+              /* Convert back to f64 */
+              .map(|d| d.into_inner())
+              .sum()
     }
 }
 
