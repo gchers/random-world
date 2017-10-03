132c71c13ced230aabf7061225d4fe0a8ffad696
Giovanni Cherubin <giovanni.cherubin@gmail.com>
2017-10-03 22:33:29 +0100

    Add benchmarks for CP train and predict

---
+++ benches/cp.rs
@@ -0,0 +1,62 @@
+#![feature(test)]
+
+extern crate test;
+extern crate confident;
+extern crate pcg_rand;
+extern crate rand;
+extern crate ndarray;
+
+use test::{Bencher, black_box};
+use pcg_rand::Pcg32;
+use rand::{Rng, SeedableRng};
+use confident::cp::*;
+use confident::ncm::*;
+use ndarray::prelude::*;
+
+fn generate_data(n: usize, d: usize, n_labels: usize, seed: [u64; 2])
+        -> (Array2<f64>, Array1<usize>) {
+    let mut rng = Pcg32::from_seed(seed);
+
+    let inputs = Array::from_iter(rng.gen_iter::<f64>()
+                                     .take(n*d)).into_shape((n, d))
+                                                .unwrap();
+    let targets = Array::from_iter((0..n).into_iter()
+                                         .map(|_| rng.gen_range::<usize>(0, n_labels)));
+    (inputs, targets)
+}
+    
+#[bench]
+fn bench_cp_train(b: &mut Bencher) {
+    let ncm = KNN::new(2);
+    let mut cp = CP::new(|x, y| ncm.score(x, y), Some(0.1));
+
+    let n = 1000;
+    let d = 2;
+    let n_labels = 2;
+    let seed = [0, 0];
+
+    let (inputs, targets) = generate_data(n, d, n_labels, seed);
+
+    b.iter(|| {
+        let _ = cp.train(&inputs, &targets);
+    });
+}
+
+#[bench]
+fn bench_cp_predict(b: &mut Bencher) {
+    let ncm = KNN::new(2);
+    let mut cp = CP::new(|x, y| ncm.score(x, y), Some(0.1));
+
+    let n = 10;
+    let d = 1;
+    let n_labels = 2;
+    let seed = [0, 0];
+
+    let (inputs, targets) = generate_data(n, d, n_labels, seed);
+
+    let _ = cp.train(&inputs, &targets);
+
+    b.iter(|| {
+        let _ = black_box(cp.predict_confidence(&inputs).unwrap());
+    });
+}
