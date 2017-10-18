a954410c071a11b1d1f40b218ffd7951151b788e
Giovanni Cherubin <giovanni.cherubin@gmail.com>
2017-10-18 17:25:01 +0100

    CP train and predict accepts ArrayView and not Array

--- src/cp/mod.rs
+++ src/cp/mod.rs
@@ -17,8 +17,8 @@ pub use self::cp::CP;
 /// This trait is parametrized over `T`, the element type.
 /// It provides all the methods for making a confidence prediction.
 pub trait ConfidencePredictor<T> {
-    fn train(&mut self, inputs: &Array2<T>, targets: &Array1<usize>) -> LearningResult<()>;
-    fn predict(&mut self, inputs: &Array2<T>) -> LearningResult<Array2<bool>>;
-    fn predict_confidence(&mut self, inputs: &Array2<T>) -> LearningResult<Array2<f64>>;
+    fn train(&mut self, inputs: &ArrayView2<T>, targets: &ArrayView1<usize>) -> LearningResult<()>;
+    fn predict(&mut self, inputs: &ArrayView2<T>) -> LearningResult<Array2<bool>>;
+    fn predict_confidence(&mut self, inputs: &ArrayView2<T>) -> LearningResult<Array2<f64>>;
     fn set_epsilon(&mut self, epsilon: f64);
 }
