7c8ce38627f77faf6b17bb6670bcd14429ff3e9b
Giovanni Cherubin <g.chers@gmail.com>
2019-03-23 20:01:31 +0100

    Add unit tests based on Iris dataset

--- tests/cp.rs
+++ tests/cp.rs
@@ -1,12 +1,15 @@
-#[macro_use(array)]
 extern crate ndarray;
+extern crate itertools;
 
 extern crate random_world;
 
 #[cfg(test)]
 mod tests {
+    use ndarray::*;
     use random_world::cp::*;
     use random_world::ncm::*;
+    use random_world::utils::*;
+    use itertools::multizip;
     
     #[test]
     fn cp() {
@@ -36,8 +39,8 @@ mod tests {
 
         cp.train(&train_inputs.view(), &train_targets.view()).unwrap();
         let pvalues = cp.predict_confidence(&test_inputs.view()).unwrap();
-        println!("Expected p-values: {:?}. P-values: {:?}.", expected_pvalues,
-                 pvalues);
+        println!("Expected p-values: {:?}", expected_pvalues);
+        println!("P-values: {:?}", pvalues);
         assert!(pvalues == expected_pvalues);
         cp.set_epsilon(epsilon_1);
         assert!(cp.predict(&test_inputs.view()).unwrap() == expected_preds_1);
@@ -71,5 +74,68 @@ mod tests {
         println!("Expected p-values: {:?}.", expected_pvalues);
         println!("Actual p-values: {:?}.", pvalues);
         assert!(pvalues == expected_pvalues);
+    }
+
+   /// Tests CP in batch mode (i.e., train on training set, predict
+   /// test set. For simplicity of the test, training and test sets are
+   /// identical.
+    #[test]
+    fn cp_batch_iris() {
+        let k = 3;
+        let ncm = KNN::new(k);
+        let n_labels = 3;
+        let mut cp = CP::new(ncm, n_labels, None);
+
+        let (train_inputs, train_targets) = load_data("tests/data/iris.csv")
+                                            .expect("Failed to load data");
+        let expected_pvalues = load_pvalues("tests/data/iris-batch-expected.csv")
+                                    .expect("Failed to load p-values");
+
+        cp.train(&train_inputs.view(), &train_targets.view())
+          .expect("Failed to train the model");
+
+        let pvalues = cp.predict_confidence(&train_inputs.view())
+                        .expect("Failed to predict");
+
+        println!("Expected p-values: {:?}.", expected_pvalues);
+        println!("Actual p-values: {:?}.", pvalues);
+        assert!(pvalues == expected_pvalues);
+    }
+
+    /// Tests CP in online mode (i.e., iteratively train on i points,
+    /// test on the i+1-th point, and add the i+1-th example into the
+    /// new training set.
+    #[test]
+    fn cp_online_iris() {
+        let k = 3;
+        let ncm = KNN::new(k);
+        let n_labels = 3;
+        let mut cp = CP::new(ncm, n_labels, None);
+
+        let (train_inputs, train_targets) = load_data("tests/data/iris.csv")
+                                            .expect("Failed to load data");
+        let expected_pvalues = load_pvalues("tests/data/iris-online-expected.csv")
+                                    .expect("Failed to load p-values");
+
+        // Train on first data point.
+        let x = train_inputs.slice(s![0..1, ..]);
+        let y = train_targets[[0]];
+        cp.train(&x, &array![y].view())
+          .expect("Failed to train CP");
+
+        // Update and predict the remaining points in on-line mode.
+        for (x, y, exp_pvals) in multizip((train_inputs.outer_iter().skip(1),
+                                           train_targets.iter().skip(1),
+                                           expected_pvalues.outer_iter())) {
+            let x_ = x.into_shape((1, x.len())).unwrap();
+            let preds = cp.predict_confidence(&x_)
+                          .expect("Failed to predict");
+            println!("Expected: {:?}", exp_pvals);
+            println!("P-values: {:?}", preds);
+            assert!(preds.row(0) == exp_pvals);
+
+            cp.update(&x_, &array![*y].view())
+              .expect("Failed to update CP");
+        }
     }
 }
